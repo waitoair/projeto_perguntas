9105b8d2aa9cd7b51cfe1e6bfd94a27763577354
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-05-21 14:02:45 -0300

    connecting function to the button event

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,16 @@ main() => runApp(const PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({Key? key}) : super(key: key);
 
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return() {
+      print('Pergunta respondida!');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     final perguntas = [
@@ -21,10 +31,18 @@ class PerguntaApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Text(perguntas[0]),
-            const ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: null,
-            )
+            ElevatedButton(
+              child: const Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: const Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: const Text('Resposta 3'),
+              onPressed: responder,
+            ),
           ]
         )
       )
--- observacoes_para_o_projeto.md
+++ observacoes_para_o_projeto.md
@@ -1,2 +1,8 @@
 
 obs1 - RaisedButton foi descontinuado no Flutter 2, usar ElevatedButton no lugar ( https://blog.cod3r.com.br/botoes-no-flutter-2 ) - link para guia dos botoes
+
+void Function() funcaoQueRetornaUmaOutraFuncao() {
+  return() {
+    print('Pergunta respondida!');
+  };
+}
