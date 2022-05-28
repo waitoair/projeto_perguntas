c4e597b6e341c2bb53a06be4f1edbb22b2393f89
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-05-27 23:12:31 -0300

    refactoring

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -19,14 +19,16 @@ class Questionario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      List<String> respostas = temPerguntaSelecionada
+      List<Map<String, Object>> respostas = temPerguntaSelecionada
       ? perguntas[perguntaSelecionada].cast()['respostas']
       : [];
-      
+       
     return Column(
       children: <Widget>[
         Questao(perguntas[perguntaSelecionada]['texto'].toString()),
-        ...respostas.map((t) => Resposta(t, quandoResponder)).toList(),
+        ...respostas
+        .map((resp) => Resposta(resp['texto'].toString(),quandoResponder))
+        .toList(),
       ],
     );
   }
